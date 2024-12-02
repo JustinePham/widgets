f2e679ebeef22062ce3a96260d7912bef9b67991
Justine Manalili Pham <tinemanalili@gmail.com>
2024-12-02 21:50:51 +0100

    feature:  update the bargraph

--- src/components/bargraph.tsx
+++ src/components/bargraph.tsx
@@ -10,14 +10,14 @@ const BarGraph: React.FC<{ data: any }> = ({ data }) => {
 
   return (
     <div className="w-full ">
-      <div className="w-full flex flex-col pt-1 pb-1">
+      <div className="w-full flex flex-col pt-1 pb-1 gap-2 items-start">
         <label className="text-xs font-bold">
-          Company information is {Math.floor(getPct(data))} % complete!
+          Item is {Math.floor(getPct(data))}% complete!
         </label>
-        <span
-          className="h-1 bg-gradient-to-r from-teal-400 to-blue-500 rounded-md pl-2 pr-2"
-          style={{ width: Math.floor(getPct(data)) + '%' }}
-        ></span>
+        <div className="rounded-md h-1 bg-green-200  inline-flex w-full">
+        <span className="h-1 bg-gradient-to-r from-teal-400 to-blue-500 rounded-md "
+          style={{ width: Math.floor(getPct(data)) + '%' }}></span>
+        </div>
       </div>
     </div>
   );
