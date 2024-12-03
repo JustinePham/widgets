5fbf8e8a48c0ca751e32e4dfd64b545562e6d7ea
Justine Manalili Pham <tinemanalili@gmail.com>
2024-12-03 15:53:35 +0100

    refactor: removed extra divs

--- src/components/barchart.tsx
+++ src/components/barchart.tsx
@@ -5,23 +5,19 @@ const BarChart: FC<{ data:{[key: string]: number}, barWidth:string, colors:strin
     const sum = Object.values(data).reduce((accumulator, current) => accumulator + current);
  
      return (
-      <div className="w-full h-full">
         <div className="h-full flex flex-row gap-1 items-end">
          { 
             sections.map((section, index) => (
-                <div className='h-full w-full'>
-                    <div className="w-full h-full flex flex-col justify-end rounded-md gap-1 items-center"  key={section}>
-                        <div className='h-4/5 flex flex-col jusitfy-end'>
-                            <label className="text-xs font-bold text-center" style={{ color: colors[index] }}>{data[section]}</label>
-                            <div className=" rounded-md" style={{ backgroundColor: colors[index], width:barWidth, height: ((data[section] / sum) * 100) + "%"  }}></div>
-                        </div>
-                        <label className="text-xs font-bold text-center h-1/5" style={{ color: colors[index] }}>{section}</label>
-                    </div>
-                </div> 
+            <div className="w-full h-full flex flex-col justify-end rounded-md gap-1 items-center"  key={section}>
+                <div className='h-4/5 flex flex-col justify-end'>
+                    <label className="text-xs font-bold text-center" style={{ color: colors[index] }}>{data[section]}</label>
+                    <div className=" rounded-md" style={{ backgroundColor: colors[index], width:barWidth, height: ((data[section] / sum) * 100) + "%"  }}></div>
+                </div>
+                <label className="text-xs font-bold text-center h-1/5" style={{ color: colors[index] }}>{section}</label>
+            </div>
             ))
          }
         </div>
-      </div>
     );
   };
   export default BarChart;
